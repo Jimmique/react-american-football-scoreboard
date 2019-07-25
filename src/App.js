02cb01812fa4486d6612f66efdbd0bab4e65a145
Jimmique <jimmique88@gmail.com>
2019-07-24 19:31:55 -0700

    Added yarn.lock, imported useState hook, and working on step 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
 
+  const buttonHandler = (name, amount) => {
+    name === 'Lions' ?
+      setHomeScore(homeScore + amount):
+      setAwayScore(awayScore + amount);
+  };
+  
   return (
     <div className="container">
       <section className="scoreboard">
